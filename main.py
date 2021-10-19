ef31c7f8b4619c6bcb8d150da0e6646a4e382385
Benjamin Siret <benjaminsiret@outlook.fr>
2021-10-19 16:18:35 +0200

    set APP-ID/KEY and headers

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import requests
+
+APP_ID = 88024513
+APP_KEY = "6c86ddf522410b2decf66097d54f1ad6"
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": APP_KEY,
+    "x-remote-user-id": 0,
+}
 
